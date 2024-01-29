8139e5317690a7a586e20eccbe31da61fb6575f5
reeruryu <h2ju1004@gmail.com>
2024-01-29 15:08:51 +0900

    add: webview2-study

---
+++ WebViewTest/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace WebViewTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(28, 77);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(247, 181);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Font = new System.Drawing.Font("굴림", 7F);
+            this.label1.Location = new System.Drawing.Point(26, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 10);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MS WebView2를 활용하여 Web 업무영역을 C# EXE 내부로 Embed하여 통일된 레이아웃을 구축하자";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(314, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "(.html)                                                           (.ocx)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "html로 전송";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(300, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 291);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webView21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WebViewTest/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace WebViewTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            // WebView2 초기화
+            await webView21.EnsureCoreWebView2Async(null);
+
+            // 로컬 HTML 파일 로드
+            string htmlPath = @"C:\Users\User\Desktop\rhj\customurl\demo\src\main\resources\templates\webview.html";
+            webView21.CoreWebView2.Navigate(htmlPath);
+
+            // WebView2 이벤트 핸들러 설정
+            webView21.CoreWebView2.WebMessageReceived += WebView_WebMessageReceived;
+        }
+
+        private void WebView_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            // WebView에서 C#으로 전송된 메시지를 처리합니다.
+            string message = e.TryGetWebMessageAsString();
+            MessageBox.Show($"Message from WebView: {message}");
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            // C#에서 WebView로 메시지를 전송합니다.
+            webView21.CoreWebView2.PostWebMessageAsString(textBox1.Text);
+
+            // 확인을 위해 메시지 박스를 표시
+            MessageBox.Show("Message sent to WebView!");
+        }
+    }
+}
